2d5d5dfa09eacb02a18f1b0e9b0388bd0b32de43
TimofeyTst <yarik.mist@gmail.com>
2023-09-09 18:47:58 +0300

    MainPage fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,15 +3,9 @@ import { EatListsBoard } from '@components/EatListsBoard';
 import { Box, CircularProgress } from '@mui/material';
 import type { FC } from 'react';
 
-import styles from './MainPage.module.css';
-
 const MainPage: FC = () => {
     const { data: profile, isLoading } = useGetProfileProfileGetQuery();
 
-    return (
-        <Box className={styles.mainPage}>
-            {isLoading ? <CircularProgress /> : <EatListsBoard eatLists={profile?.lists} />}
-        </Box>
-    );
+    return <Box>{isLoading ? <CircularProgress /> : <EatListsBoard eatLists={profile?.lists} />}</Box>;
 };
 export default MainPage;
